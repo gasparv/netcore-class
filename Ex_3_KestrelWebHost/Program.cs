9b08f2207968133e0235a57e5cf4b3e0200de282
Gaspar <vladimir.gaspar@tuke.sk>
2018-06-22 19:47:29 +0200

    Kestrel webhost - light implementation in console project

---
+++ Ex_3_KestrelWebHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace Ex_3_KestrelWebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Requires Microsoft.AspNetCore and Microsoft.AspNetCore.Hosting nuget packages
+            WebHost
+                //Allows to bing various startup arguments
+                .CreateDefaultBuilder(args)
+                //Specifies that kestrel server will be used. Here you can add configurations with a lambda expression
+                .UseKestrel()
+                //Mandatory specification of a Startup class
+                .UseStartup<Startup>()              
+                //Builds the webhost
+                .Build()
+                //Runs the webhost in an infinite loop (http request listener)
+                .Run();
+        }
+    }
+}
---
+++ Ex_3_KestrelWebHost/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ex_3_KestrelWebHost
+{
+    public class Startup
+    {
+        //Usually IConfiguration and IHostingEnvironment are also injected in the context using the constructor injection method
+        //IConfiguration is used to store and load different configurations from a special json configuration file - discussed later in lectures
+        //IHostingEnvironment is used to manage hosting environments and behaviour of the application in different deployments (production, staging, development, etc.) - discussed later in lectures
+        /*
+        public IHostingEnvironment HostingEnvironment { get; }
+        public IConfiguration Configuration { get; }
+        public Startup(IHostingEnvironment env, IConfiguration config)
+        {
+            HostingEnvironment = env;
+            Configuration = config;
+        }
+        */
+
+        //Requires Microsoft.Extensions.DependencyInjection nuget package
+        //This method serves for all service injections in Asp.Net core - notice the IServiceCollection as the input of the method
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Requires Microsoft.AspNetCore.Mvc nuget package
+            //Registers multiple services that allow the usage of Mvc
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            //configures the MVC as a terminating middleware
+            app.UseMvc();
+        }
+    }
+}
