a847b3509bc95239da464ece8e96bfc0da05ff17
Gaspar <vladimir.gaspar@tuke.sk>
2018-06-24 15:29:22 +0200

    Using different hosting environments

---
+++ Ex_6_HostingEnvironment/Ex_6_HostingEnvironment/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ex_6_HostingEnvironment.Models;
+
+namespace Ex_6_HostingEnvironment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new StackOverflowException();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ex_6_HostingEnvironment/Ex_6_HostingEnvironment/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ex_6_HostingEnvironment
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        //Default Configure method that is called in case the environment that is selected does not have a specific environment Configure method provided 
+        //(Development and staging in this case)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                Console.WriteLine("Is in development environment");
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        //Environment specific Configure method for the production environment.
+        //The UseBrowserLink and UseDeveloperExceptionPage middlewares are omitted in this case.
+        public void ConfigureProduction(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsProduction())
+            {
+                Console.WriteLine("Is Production environment");
+                app.UseExceptionHandler("/Home/Error");
+                app.UseStaticFiles();
+                app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+
+                });
+                
+            }
+        }
+    }
+}
