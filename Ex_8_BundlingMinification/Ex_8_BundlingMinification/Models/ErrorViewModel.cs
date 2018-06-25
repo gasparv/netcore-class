8944e6915bc1a924bf398ac060fcc7fa0e120cd9
Gaspar <vladimir.gaspar@tuke.sk>
2018-06-25 22:13:31 +0200

    Bundling and minification example

---
+++ Ex_8_BundlingMinification/Ex_8_BundlingMinification/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ex_8_BundlingMinification.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
