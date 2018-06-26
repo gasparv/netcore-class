d62d029f76cad16ddfb67a5ebee3c9ee15d649b4
Gaspar <vladimir.gaspar@tuke.sk>
2018-06-26 13:15:00 +0200

    Default ASP.NET Core TagHelpers

---
+++ Ex_10_DefaultTagHelpers/Ex_10_DefaultTagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ex_10_DefaultTagHelpers.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Ex_10_DefaultTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View(new DemonstrationTagHelperViewModel());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact(string contactName)
+        {
+            if(String.IsNullOrEmpty(contactName))
+                ViewData["Message"] = $"This is the contact page.";
+            else
+                ViewData["Message"] = $"This is your contact page {contactName}.";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        /// <summary>
+        /// //Action decorated by HttpPost for form submission calling the Index action
+        /// </summary>
+        /// <param name="data">data sent from the front-end form by submission</param>
+        /// <returns>Error status code 418 ImATeapot - just for fun :)</returns>
+        [HttpPost]
+        public IActionResult Index(DemonstrationTagHelperViewModel model)
+        {
+            if(ModelState.IsValid)
+            return StatusCode(StatusCodes.Status418ImATeapot);
+            else
+                return StatusCode(StatusCodes.Status400BadRequest);
+        }
+    }
+}
---
+++ Ex_10_DefaultTagHelpers/Ex_10_DefaultTagHelpers/Models/DemonstrationTagHelperViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_10_DefaultTagHelpers.Models
+{
+    public class DemonstrationTagHelperViewModel
+    {
+        public int IntegerValue { get; set; }
+        [Required]
+        public string StringValue { get; set; }
+
+        public IEnumerable<SelectListItem> ListCollection
+        {
+            get =>
+                new List<SelectListItem>
+                {
+                    new SelectListItem(){
+                        Text = "a",
+                        Value = "a"
+                    },
+                    new SelectListItem(){
+                        Text = "b",
+                        Value = "b"
+                    },
+                    new SelectListItem(){
+                        Text = "c",
+                        Value = "c"
+                    },
+                    new SelectListItem(){
+                        Text = "d",
+                        Value = "d"
+                    }
+                };
+        }
+    }
+}
