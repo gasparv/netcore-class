2849f118f39cb8cefe7486221470a7f0fdada16d
Gaspar <vladimir.gaspar@tuke.sk>
2018-07-09 23:00:22 +0200

    docker services for multiple projects (e.g. web & webapi)

---
+++ Ex_Ad_18_1_DockerServices/Ex_Ad_18_1_DockerServices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ex_Ad_18_1_DockerServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
